06db8de33f9b092b58437df40b5a341833ca9948
hellomac87 <hellomac87@gmail.com>
2019-02-12 09:54:00 +0900

    Redux: Fetching Data on Route Change

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,12 +1,38 @@
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { withRouter } from "react-router-dom";
 import { toggleTodo } from "../actions";
 import { getVisibleTodos } from "../reducers";
 import TodoList from "./TodoList";
+import { fetchTodos } from "../api";
+
+class VisibleTodoList extends Component {
+  componentDidMount() {
+    const { filter } = this.props;
+    fetchTodos(filter).then(todos => {
+      console.log(filter, todos);
+    });
+  }
+
+  componentDidUpdate(prevProps) {
+    const { filter } = this.props;
+    if (filter !== prevProps.filter) {
+      fetchTodos(filter).then(todos => {
+        console.log(filter, todos);
+      });
+    }
+  }
+
+  render() {
+    return <TodoList {...this.props} />;
+  }
+}
 
 const mapStateToProps = (state, { match: { params } }) => {
+  const filter = params.filter || "all";
   return {
-    todos: getVisibleTodos(state, params.filter || "all")
+    todos: getVisibleTodos(state, filter),
+    filter
   };
 };
 
@@ -18,11 +44,11 @@ const mapStateToProps = (state, { match: { params } }) => {
 //   };
 // };
 
-const VisibleTodoList = withRouter(
+VisibleTodoList = withRouter(
   connect(
     mapStateToProps,
     { onTodoClick: toggleTodo }
-  )(TodoList)
+  )(VisibleTodoList)
 );
 
 export default VisibleTodoList;
