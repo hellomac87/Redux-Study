f9c1da5784f6d1e7b2792c41264b549f629106db
hellomac87 <hellomac87@gmail.com>
2019-02-14 23:59:41 +0900

    redux-wrapping-dispatch-to-recognize-promises

--- src/configureStore.js
+++ src/configureStore.js
@@ -2,6 +2,7 @@ import { createStore } from "redux";
 import todoApp from "./reducers";
 
 const addLoggingToDispatch = store => {
+  /* eslint-disable no-console */
   const rawDispatch = store.dispatch;
   if (!console.group) {
     return rawDispatch;
@@ -12,18 +13,32 @@ const addLoggingToDispatch = store => {
     console.log("%c prev state", "color: gray", store.getState());
     console.log("%c action", "color: blue", action);
     const returnValue = rawDispatch(action);
-    console.log("%c next state", "color:green", store.getState());
+    console.log("%c next state", "color: green", store.getState());
     console.groupEnd(action.type);
-    console.group(action.type);
     return returnValue;
+  };
+  /* eslint-enable no-console */
+};
+
+const addPromiseSupportToDispatch = store => {
+  const rawDispatch = store.dispatch;
+  return action => {
+    console.log(action.then);
+    if (typeof action.then === "function") {
+      return action.then(rawDispatch);
+    }
+    return rawDispatch(action);
   };
 };
 
 const configureStore = () => {
   const store = createStore(todoApp);
+
   if (process.env.NODE_ENV !== "production") {
     store.dispatch = addLoggingToDispatch(store);
   }
+
+  store.dispatch = addPromiseSupportToDispatch(store);
 
   return store;
 };
